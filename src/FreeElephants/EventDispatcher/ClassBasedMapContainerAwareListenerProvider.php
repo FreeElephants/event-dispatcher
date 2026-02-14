76cd85fa81fb2173763b387a073e09a56b3bd715
n.gnato <n.gnato@ya.ru>
2026-02-14 15:24:32 +0300

    Add deps

---
+++ src/FreeElephants/EventDispatcher/ClassBasedMapContainerAwareListenerProvider.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace FreeElephants\EventDispatcher;
+
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+
+class ClassBasedMapContainerAwareListenerProvider implements ListenerProviderInterface
+{
+
+    private ContainerInterface $container;
+    private array $eventsMap = [];
+
+    public function __construct(
+        ContainerInterface $container,
+        array $eventsMap
+    )
+    {
+        $this->container = $container;
+        $this->eventsMap = $eventsMap;
+    }
+
+    public function getListenersForEvent(object $event): iterable
+    {
+        $listeners = $this->eventsMap[get_class($event)] ?? [];
+        return array_map(fn(string $listener) => $this->container->get($listener), $listeners);
+    }
+}
---
+++ src/FreeElephants/EventDispatcher/EventDispatcher.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace FreeElephants\EventDispatcher;
+
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+    private ListenerProviderInterface $listenerProvider;
+
+    public function __construct(
+        ListenerProviderInterface $listenerProvider
+    )
+    {
+        $this->listenerProvider = $listenerProvider;
+    }
+
+    public function dispatch(object $event)
+    {
+        foreach ($this->listenerProvider->getListenersForEvent($event) as $listener) {
+            $listener($event);
+            if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+                break;
+            }
+        }
+    }
+}
